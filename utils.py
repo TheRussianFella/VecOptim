d13b3832926469e9c0d9a7ec4c1e8ef388e6010e
TheRussianFella <maximmanainen@gmail.com>
2018-05-06 16:28:00 +0300

    start

---
+++ utils.py
@@ -0,0 +1,28 @@
+import os
+from PIL import Image
+import numpy as np
+
+def load_dir(path, shape=[400, 400]):
+    data = []
+    for i in os.listdir(path):
+        try:
+            img = np.array(Image.open(os.path.join(path, i)).resize(shape))
+            if len(img.shape) != 3:
+                img = np.stack((img,)*3, -1)
+            if img.shape[-1] != 3:
+                continue
+            data.append(img)
+        except:
+            1
+    return np.array(data)
+
+def load_dirs(path, shape=[400, 400]):
+    data = []
+
+    for dir in os.listdir(path):
+        if os.path.isdir(os.path.join(path, dir)):
+            data.extend(load_dir(os.path.join(path, dir)))
+
+    data = np.array(data)
+    
+    return data
